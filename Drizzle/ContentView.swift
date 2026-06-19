974b9a2b1cb30e8cfa39e29e78a41aa3cdc00582
osama <Osamakhaled31415@gmail.com>
2026-06-19 15:56:34 +0300

    Initial Commit

---
+++ Drizzle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drizzle
+//
+//  Created by Osama Khaled on 19/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
